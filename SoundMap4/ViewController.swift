808c8a594081c9d1686d86c8df9d271a3fb24f38
loadedjd <williamsjared62@gmail.com>
2017-09-12 22:28:31 -0400

    Initial Commit

---
+++ SoundMap4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundMap4
+//
+//  Created by Jared Williams on 9/12/17.
+//  Copyright © 2017 Jared Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
