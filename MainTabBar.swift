1626d498bb0abf82e4d4fedd761edb4d7cbe9a57
loadedjd <williamsjared62@gmail.com>
2017-09-15 18:41:46 -0400

    Added red tab bar tint color red

--- MainTabBar.swift
+++ MainTabBar.swift
@@ -26,18 +26,30 @@ class MainTabBar: UITabBarController {
         let recordNavigationController = Helper.createNavigationBar(viewContoller: recordController, title: "Records", barColor: UIColor.red, tabBarTitle:
             "Records")
         recordNavigationController.tabBarItem.image = #imageLiteral(resourceName: "recordsSymbol5")
-        recordNavigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: UIColor.white]
+        recordNavigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey(rawValue: NSAttributedStringKey.foregroundColor.rawValue): UIColor.white]
+        if #available(iOS 11.0, *) {
+            recordNavigationController.navigationBar.prefersLargeTitles = true
+        } else {
+            // Fallback on earlier versions
+        }
         
         
         let mapNavigationController = Helper.createNavigationBar(viewContoller: MapController(), title: "Map", barColor: UIColor.red, tabBarTitle: "Map")
         mapNavigationController.tabBarItem.image = #imageLiteral(resourceName: "locationSymbol")
-        mapNavigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: UIColor.white]
+        mapNavigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey(rawValue: NSAttributedStringKey.foregroundColor.rawValue): UIColor.white]
         
         let settingsNavigationController = Helper.createNavigationBar(viewContoller: SettingsController(), title: "Settings", barColor: UIColor.red, tabBarTitle: "Settings")
         settingsNavigationController.tabBarItem.image = #imageLiteral(resourceName: "if_settings_115801")
-        settingsNavigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: UIColor.white]
+        settingsNavigationController.navigationBar.titleTextAttributes = [NSAttributedStringKey(rawValue: NSAttributedStringKey.foregroundColor.rawValue): UIColor.white]
+        if #available(iOS 11.0, *) {
+            settingsNavigationController.navigationBar.prefersLargeTitles = true
+        } else {
+            // Fallback on earlier versions
+        }
         
         self.viewControllers = [recordNavigationController, mapNavigationController, settingsNavigationController]
+        
+        self.tabBar.tintColor = UIColor.red
     }
     
 }
